e3505d573454371ff95897da7f3af80c6ebd6605
Ricardo Navarro <135258542+Ricardo-Navarro@users.noreply.github.com>
2024-01-22 15:36:50 -0300

    Add files via upload

---
+++ MyRazorApp/Pages/Index.cshml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyRazorApp.Pages;
+
+public class Index : PageModel
+{
+  public void OnGet()
+  {
+    
+  }
+}
+
